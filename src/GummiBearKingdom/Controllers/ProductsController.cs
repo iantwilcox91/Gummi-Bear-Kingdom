492831e8e052c2e2fd7aec00dad1a165731795cf
ian Wilcox <ian Wilcox>
2016-11-18 16:23:28 -0800

    can view products/details

---
+++ src/GummiBearKingdom/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GummiBearKingdom.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GummiBearKingdom.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: /<controller>/
+        private GummiBearKingdomContext db = new GummiBearKingdomContext();
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductsId == id);
+            return View(thisProduct);
+        }
+    }
+}
---
+++ src/GummiBearKingdom/Models/GummiBearKingdomContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GummiBearKingdom.Models
+{
+    public class GummiBearKingdomContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GummiBearKingdom;integrated security=True");
+        }
+    }
+}
